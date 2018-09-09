bc9791d2ec8e31f228158129d667586c99a248df
Ben Glassman <ben@symbioticlabs.io>
2018-09-08 22:42:37 -0400

    API and Web connected.

--- http-api/server.js
+++ http-api/server.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var cors = require('cors');
 var { getNameSuggestions } = require('./namerator');
 
 var app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get('/suggestions', function(req, res) {
   const words = req.query.words;
